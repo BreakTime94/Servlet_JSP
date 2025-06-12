3352d834a0c42220097cfa0daf499f01d3aab861
kimheeseon0526 <kimchan9408@gmail.com>
2025-06-12 21:14:59 +0900

    250612

---
+++ pbl/src/main/java/controller/member/Register.java
@@ -0,0 +1,45 @@
+package controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import service.MemberService;
+
+@WebServlet("/member/register")
+@Slf4j
+public class Register extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/member/register.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//1. 파라미터 수집
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		log.info("{} {} {} {}", id, pw, name, email);
+		//2. Member인스턴스 생성
+		Member member = Member.builder().id(id).pw(pw).name(name).email(email).build();
+		log.info("{}", member);
+		new MemberService().register(member);
+		
+		//3. memberservice.register(member)호출
+		
+		//4. index로 리디렉션
+		resp.sendRedirect("../index");
+	
+	}
+	
+}
--- pbl/src/main/java/service/MemberService.java
+++ pbl/src/main/java/service/MemberService.java
@@ -3,14 +3,18 @@ package service;
 import org.apache.ibatis.session.SqlSession;
 
 import domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import mapper.MemberMapper;
 import util.MybatisUtil;
+import util.PasswordEncoder;
 
+@Slf4j
 public class MemberService {
 	public int register(Member member) {
 		try(SqlSession session = MybatisUtil.getSqlSession()){
 			
 			MemberMapper mapper = session.getMapper(MemberMapper.class);
+			member.setPw(PasswordEncoder.encode(member.getPw())); 
 			return mapper.insert(member);
 			
 		}
@@ -30,12 +34,25 @@ public class MemberService {
 			e.printStackTrace();
 		}
 		return null;
-	}	
+	}
+	public boolean login(String id, String pw) {
+		Member member = findById(id);
+		if(member == null) {
+			return false;
+		}
+		return PasswordEncoder.matches(pw, member.getPw());
+		
+	}
+	
 	public static void main(String[] args) {
 		MemberService memberService = new MemberService();
 		
-		Member member = Member.builder().id("sae1").pw("1234").build();
+		log.info("{}", memberService.findById("sae"));
+		log.info("{} 핫하 {}", 10, 20); //printf와 유사하다.
 		
-		memberService.register(member);
+//		System.out.println(memberService.login("sae", "1234"));
+		log.info("{}", memberService.login("sae", "1234"));
+		log.error("{}", memberService.login("sae", "12345"));
+//		memberService.register(Member.builder().id("sae3").pw("1234").name("말똥이").build());
 	}
 }
